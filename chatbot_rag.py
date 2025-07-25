4ade39b6ab941297363f7dd6658bd3593063311c
manikolan <manikolan344@gmail.com>
2025-07-25 16:06:42 -0500

    Update chatbot title

--- chatbot_rag.py
+++ chatbot_rag.py
@@ -1,4 +1,3 @@
-#import streamlit
 import streamlit as st
 import os
 from dotenv import load_dotenv
@@ -61,10 +60,10 @@ if prompt:
     for match in response["matches"]:
         docs_text += match["metadata"].get("text", "") + "\n"
 
-
     # Create the system prompt
-    system_prompt = f"""You are an AI assistant that has information about the document provided below.\
-    If the question has nothing to do with the context, give generic answers like a normal AI Assistant. Thank you!\nContext: {docs_text}"""
+    system_prompt = f"""You are an AI assistant that has information about the document provided below.\n
+    If the question has nothing to do with the context, give generic answers like a normal AI Assistant. Thank you!
+Context: {docs_text}"""
 
     st.session_state.messages.append(SystemMessage(system_prompt))
 
